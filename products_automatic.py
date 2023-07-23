42ddd54684ec75a86ec1fd504e5095fe9fbfca8e
José A. Galindo <jagalindo@us.es>
2023-07-23 23:28:40 +0200

    basic example

---
+++ products_automatic.py
@@ -0,0 +1,10 @@
+from flamapy.core.discover import DiscoverMetamodels # This loads the tool in the python execution environment
+dm = DiscoverMetamodels()
+#result = dm.use_operation_from_file("Products", "./tutorial-splc22/models/pizzas/pizzas.fide")
+#result = dm.use_operation_from_file("ValidProduct", "./tutorial-main/models/pizzas/pizzas.fide",configuration_file="./tutorial-main/models/pizzas_nonvalid.csvconf")
+print(dm.get_plugins())
+#result = dm.use_operation_from_file("Valid", "./pizzas.uvl")
+result = dm.use_operation_from_file("Glucose3Diagnosis", "./pizzas.uvl" ,configuration_file="./pizzas_nonvalid.csvconf")
+
+
+print(result)
