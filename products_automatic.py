ee139640ac7d144498ddefd6e0928e016a9d5fec
José A. Galindo <jagalindo@us.es>
2023-08-23 18:13:59 +0200

    Updating example

--- products_automatic.py
+++ products_automatic.py
@@ -4,7 +4,9 @@ dm = DiscoverMetamodels()
 #result = dm.use_operation_from_file("ValidProduct", "./tutorial-main/models/pizzas/pizzas.fide",configuration_file="./tutorial-main/models/pizzas_nonvalid.csvconf")
 print(dm.get_plugins())
 #result = dm.use_operation_from_file("Valid", "./pizzas.uvl")
-result = dm.use_operation_from_file("Glucose3Diagnosis", "./pizzas.uvl" ,configuration_file="./pizzas_nonvalid.csvconf")
+#result = dm.use_operation_from_file("Glucose3Diagnosis", "./pizzas.uvl" ,configuration_file="./pizzas_nonvalid.csvconf")
 
+#result = dm.use_operation_from_file("Glucose3Conflict", "./pizzas.uvl" ,configuration_file="./pizzas_nonvalid.csvconf")
+result = dm.use_operation_from_file("Metrics", "./pizzas.uvl")
 
 print(result)
